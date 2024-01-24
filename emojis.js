da1c0daca6df22d02a53e29e39481023e45bfb42
essyk <essyking@gmail.com>
2024-01-24 21:44:51 +0300

    Create emojis.js

---
+++ emojis.js
@@ -0,0 +1,22 @@
+const emojis = [
+  {
+    text: "Awful",
+    url: "https://essykings.github.io/JavaScript/svg-emojis/Cry.svg"
+  },
+ {
+    text: "Bad",
+    url: "https://essykings.github.io/JavaScript/svg-emojis/Disappointed.svg"
+  },
+  {
+    text: "Okay",
+    url: "https://essykings.github.io/JavaScript/svg-emojis/Expressionless.svg"
+  },
+  {
+    text: "Good",
+    url: "https://essykings.github.io/JavaScript/svg-emojis/Smile.svg"
+  },
+  {
+    text: "Great",
+    url: "https://essykings.github.io/JavaScript/svg-emojis/Beaming_smile.svg"
+  }]
+  
